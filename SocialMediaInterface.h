baf2b07c4f8f80a047097d06c4295952f7cd690c
Mouni3688 <mounika.reddy.golla@gmail.com>
2025-01-20 10:20:12 +0530

    Add files via upload

---
+++ SocialMediaInterface.h
@@ -0,0 +1,16 @@
+
+#ifndef ARDUINO_SOCIALMEDIAINTERFACE_H
+#define ARDUINO_SOCIALMEDIAINTERFACE_H
+#include <Arduino.h>
+
+class SocialMediaInterface  
+{
+	private:
+
+	public:
+
+		SocialMediaInterface();
+		~SocialMediaInterface();
+
+};
+#endif
